698d842470d1f0610b4d898af112ea130681bcdd
nickbartram <nickbartram25@gmail.com>
2025-11-26 14:03:50 -0500

    Connected to RDS

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
+
+from postgres import postgres_user, postgres_pass
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+def create_app():
+    app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f"postgresql://{postgres_user}:{postgres_pass}"
+        f"@climate-db.croamw4iqxpi.us-east-2.rds.amazonaws.com:5432/postgres"
+    )
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        # Use a connection context
+        with db.engine.connect() as conn:
+            result = conn.execute(text("SELECT version();"))
+            print(result.fetchone())
+
